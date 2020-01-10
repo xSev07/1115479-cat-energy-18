baa887847a09cb159c3fa046b55b1c1729bc917a
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-01-10 21:48:50 +0500

    Minor fixes

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,7 @@ gulp.task("webp", function () {
 })
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/sprite/icon-*.svg")
+  return gulp.src("source/img/sprite/*.svg")
     .pipe(svgstore({inlineSvg: true}))
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img"));
